27c0f3c8dda5db2beb2097faaa2ca0e2c37a7e1e
kknoerz <kevink430@gmail.com>
2016-03-23 17:52:35 -0400

    separated logic/html

--- ap.js
+++ ap.js
@@ -1,17 +1,131 @@
 
 
- //stores the name of the button into var select
-// var queryURL = "http://api.yelp.com/v2/search/?" + what + '&' + where
 
-// $.ajax({ 
-//                 url: queryURL,
-//                 method: 'GET'
-//             })
-//             .done(function(response) {
 
-//                 console.log(response)
-//                 var results = response.data
-//             });
+//****************************************** Facebook SDK ******************************************************
+
+
+
+  // This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1518819868427496',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.5' // use graph api version 2.5
+  });
+
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+
+
+////***************************** firebase facebook user auth ******************************************
+
+
+            // Create a callback which logs the current auth state
+function authDataCallback(authData) {
+  if (authData) {
+    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+  } else {
+    console.log("User is logged out");
+  }
+}
+
+// Register the callback to be fired every time auth state changes
+var ref = new Firebase("https://sizzling-heat-1076.firebaseio.com/");
+ref.onAuth(authDataCallback);
+
+$(function() {
+    $('#fbLogin').click(function(){
+        // debugger;
+
+        var ref = new Firebase("https://sizzling-heat-1076.firebaseio.com");
+        // prefer pop-ups, so we don't navigate away from the page
+            ref.authWithOAuthPopup("facebook", function(error, authData) {
+              if (error) {
+                if (error.code === "TRANSPORT_UNAVAILABLE") {
+                  // fall-back to browser redirects, and pick up the session
+                  // automatically when we come back to the origin page
+                  ref.authWithOAuthRedirect("facebook", function(error) { /* ... */ });
+                }
+              } else if (authData) {
+                console.log(authData)
+                // user authenticated with Firebase
+              }
+            });
+
+    });
+});
+
+
+//****************************************** Yelp ******************************************************       
 
 $('#submit').on('click', function(){
     
