b3aab03b586600395c5581b705f1548884fdd08a
kknoerz <kevink430@gmail.com>
2016-03-23 13:16:42 -0400

    cleaned ajax

--- ap.js
+++ ap.js
@@ -1,16 +1,2 @@
 
-    var type = 'Jaws';
-    var queryURL = "http://www.omdbapi.com/?t=" +  + "&y=&plot=short&r=json";
 
-    $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-    	console.log(response)
-
-    	$('#title').html(response.Title);
-    	$('#plot').html(response.Plot)
-    	$('#image').attr("src", response.Poster);
-
-
-    }); 
-    //movie title
-    //plot
-    //image
