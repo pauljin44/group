689e7a3385694dc082283db16efd6fcb1c76676e
kknoerz <kevink430@gmail.com>
2016-03-24 20:55:01 -0400

    emails

--- ap.js
+++ ap.js
@@ -78,33 +78,18 @@
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
     console.log('Welcome!  Facebook SDK is fetching your information.... ');
-    FB.api('/me?fields=id,name,email,permissions', function(response) {
+    FB.api('/me?fields=id,name,emails,permissions', function(response) {
         console.log(response) //
         console.log('Successful Facebook login for: ' + response.name);
         console.log(response.email)
         document.getElementById('status').innerHTML =
             'Thanks for logging in, ' + response.name + '!';
     },
-    {scope:'email'}
+    {scope:'emails'}
     );
   }
 
-// function testAPI() {
-//     FB.login(
-//         function(response) {
-//             if (response.authResponse) {
-//                console.log('Welcome!  Fetching your information.... ');
-//                FB.api('/me', function(response) {
-//                    console.log('Good to see you, ' + response.email + '.');
-//                    alert('Good to see you, ' + response.email + '.');
-//                });
-//             } else {
-//                 console.log('User cancelled login or did not fully authorize.');
-//             }
-//         },
-//         {scope:'email'}
-//         );
-// }
+
 
 
 
