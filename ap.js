eea1e4d0c352b132064e6798957309d79fe54df3
kknoerz <kevink430@gmail.com>
2016-03-25 16:25:35 -0400

    changes

--- ap.js
+++ ap.js
@@ -80,12 +80,16 @@
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
+    // console.log('Welcome!  Fetching your information.... ');
+    // FB.api('/me', function(response) {
+    //   console.log('Successful login for: ' + response.name);
+    //   document.getElementById('status').innerHTML =
+    //     'Thanks for logging in, ' + response.name + '!';
+    // });
+    FB.api('/me','GET', {"fields":"id,name,email,likes"},function(response) {
+      console.log('response is api response', response)
+  }
+);
   }
 
 
