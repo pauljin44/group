98aa8906a0e46c196f1c21a1bd4d704bdbf6c5a0
kknoerz <kevink430@gmail.com>
2016-03-25 23:30:57 -0400

    changes

--- ap.js
+++ ap.js
@@ -79,6 +79,7 @@ var FBresponse
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
+  var FBwhat = []
   function testAPI() {
     // console.log('Welcome!  Fetching your information.... ');
     // FB.api('/me', function(response) {
@@ -92,6 +93,7 @@ var FBresponse
           var FBwhat = FBresponse.likes.data[i].name
 
           console.log(FBwhat)
+
           } 
       });
   }
