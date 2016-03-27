e6230ded8cda7d5fa07edd14ccc859acae23b602
kknoerz <kevink430@gmail.com>
2016-03-27 10:04:52 -0400

    changes

--- ap.js
+++ ap.js
@@ -191,7 +191,7 @@ function runYelpOnce() {
 
         for(i=0; i<=1; i= i+1){
 
-                    $("#FB_likes").append("<tr id='likesRow'>"   +   '<td>'+'<a href = '+data.businesses[i].url+"</a>"+'</td>'   +   '<td>'+'<a>'+data.businesses[i].name +'</a>'+'</td>'+  '</tr>')
+                    $("#FB_likes").append("<tr class='likesRow'>"   +   '<td>'+'<a href = '+data.businesses[i].url+"</a>"+'</td>'   +   '<td>'+'<a>'+data.businesses[i].name +'</a>'+'</td>'+  '</tr>')
                     $("#likesRow").append('<tr>'   +   '<td>'+'<img src='+ data.businesses[i].rating_img_url+'>'+'</td>'   +   '<td>'+'<img src='+data.businesses[i].image_url+'>'+'</td>'+'</tr>');
                     $("#likesRow").append('<td>').attr('value','Phone: ').attr('value', data.businesses[i].phone);
                     $("#likesRow").append("<td>").attr('value', 'Yelp Reviews: ').attr('value', data.businesses[i].review_count); 
