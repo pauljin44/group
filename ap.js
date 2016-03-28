2715562ba125e2d2558158f264ddcd8591da834d
kknoerz <kevink430@gmail.com>
2016-03-28 18:33:53 -0400

    changes

--- ap.js
+++ ap.js
@@ -87,13 +87,13 @@ $('#yelpSearches').hide();
           var fbAllFriendsList = fbPaging.replace('limit=25', 'limit=5000');
           console.log(fbAllFriendsList);
 
-          $.ajax({ 
-          url: fbAllFriendsList,
-          method: 'GET'
-            })
-            .done(function(response) {
-            	console.log(response)
-            });
+          // $.ajax({ 
+          // url: fbAllFriendsList,
+          // method: 'GET'
+          //   })
+          //   .done(function(response) {
+          //   	console.log(response)
+          //   });
 
 					
 
