6575ed51acbd72400d9ff393ba1db1af85ff4876
kknoerz <kevink430@gmail.com>
2016-03-28 13:25:11 -0400

    changes

--- ap.js
+++ ap.js
@@ -284,7 +284,7 @@ function runYelp() {
 				console.log(data);
 
 
-				$('.panel-title').text('The best '+what+' spots in '+where+' are listed below');
+				$('.panel-title').text('The best '+what+' spots in '+where+' are listed below').css('text-align', 'center');
 				$("#searches").addClass('table table-hover')
 		
 		var i;
