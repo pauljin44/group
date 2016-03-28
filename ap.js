7642d41db98f73e1b47a942e8cfd53f51e8294ce
kknoerz <kevink430@gmail.com>
2016-03-28 01:24:53 -0400

    changes

--- ap.js
+++ ap.js
@@ -204,12 +204,11 @@ function runYelpOnce() { //The function runs one time for every FB 'like'
 
 		for(i=0; i<=1; i= i+1){
 
-								$("#searches").append("<tr class='likesRow'>"   +   '<td>'+'<a href = '+data.businesses[i].url+"</a>"+'</td>'   +   '<td>'+'<a>'+data.businesses[i].name +'</a>'+'</td>'+  '</tr>')
-								$("#likesRow").append('<tr>'   +   '<td>'+'<img src='+ data.businesses[i].rating_img_url+'>'+'</td>'   +   '<td>'+'<img src='+data.businesses[i].image_url+'>'+'</td>'+'</tr>');
-								$("#likesRow").append('<td>').attr('value','Phone: ').attr('value', data.businesses[i].phone);
-								$("#likesRow").append("<td>").attr('value', 'Yelp Reviews: ').attr('value', data.businesses[i].review_count); 
-								$("#likesRow").append("<br />"); 
-			 
+							$("#searches").append("<tr class="+i+">"+'<td>'+'<a href='+data.businesses[i].url+">"+data.businesses[i].name +"</a>"+'</td>'+'</tr>');
+								$("."+i).append('<tr>'   +   '<td>'+'<img src='+ data.businesses[i].rating_img_url+'>'+'</td>'   +   '<td>'+'<img src='+data.businesses[i].image_url+'>'+'</td>'+'</tr>');
+								$("."+i).append('<td>').attr('value','Phone: ').attr('value', data.businesses[i].phone);
+								$("."+i).append("<td>").attr('value', 'Yelp Reviews: ').attr('value', data.businesses[i].review_count); 
+								$("."+i).append("<br />"); 
 								
 
 			 }
