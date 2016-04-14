21c3282f0c3fda79aa4c495b691026abb2354e6e
kknoerz <kevink430@gmail.com>
2016-04-14 11:40:53 -0400

    push address and phone into firebase with ate here button

--- assets/javascript/ap.js
+++ assets/javascript/ap.js
@@ -6,11 +6,11 @@
 //****************************************** Facebook SDK ******************************************************
 var firebaseValueCheck = new Firebase("https://sizzling-heat-1076.firebaseio.com/");
 var firebaseCountUp = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/");
-var FBwhat = []; 
-var FBwhere = []; 
-var FBYelpwhat; 
+var FBwhat = [];
+var FBwhere = [];
+var FBYelpwhat;
 var FBresponse;
-// var where = [] 
+// var where = []
 var fbAllFriendsList;
 var allFriends = [];
 var allFriendImg = [];
@@ -21,7 +21,7 @@ var currentUser;
 
 $(document).ready(function(){
 
-//************************* Submit function ************************************ 
+//************************* Submit function ************************************
     $('#submit').on('click', function() {
         // debugger;
         $('#searches').empty();
@@ -31,7 +31,7 @@ $(document).ready(function(){
         return false
 
     });
- 
+
 
     $('.dropdown').on('show.bs.dropdown', function(){
         var firebasePointsValue = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/"+currentUser+"/count");
@@ -40,8 +40,8 @@ $(document).ready(function(){
             if (snapshot.val()===null)
                 {$('#points').append('<li><p>Make an account or log in to track your LocalePoints!</p></li>');
             } else{
-                $('#points').append('<li><p>You have '+snapshot.val()+' LocalePoints!</p></li>') 
-            }   
+                $('#points').append('<li><p>You have '+snapshot.val()+' LocalePoints!</p></li>')
+            }
         });
 
         var firebasePlacesValue = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/"+currentUser+"/places");
@@ -68,47 +68,47 @@ $(document).ready(function(){
 $('#modalOld').ready(function(){
     $('#oldUserSubmit').on('click', function() {
         console.log('working');
-        
-        
+
+
         var test = $('#oldUser').val()
 
         var firebaseOldUser = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/"+test);
 
         firebaseOldUser.on('value', function(snapshot){
-            
-            
+
+
             if (snapshot.val() != undefined) {
-             
-             currentUser = $('#oldUser').val();   
+
+             currentUser = $('#oldUser').val();
                 console.log('currentUser is: ', currentUser);
             }else{
                 console.log('currentUser login unsucessful')
             }
             currentUserFirebase = firebaseCountUp.child(currentUser);
             currentUserFirebase.on('value', function(snapshot){
-    
+
                 if (snapshot.val().count != undefined) {
                     localCounter = snapshot.val().count
                 }
-                
+
                 if (snapshot.val().places != undefined){
                     checkedPlaces = snapshot.val().places
                 }
 
                 updateCounter();
-            
+
             });
 
 
 });
 
-        
 
-        
+
+
         $('#oldUserSubmit').hide();
 
-        
-    });    
+
+    });
 });
 
 
@@ -120,24 +120,24 @@ $('#modalNew').ready(function(){
         var firebaseNewUser = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/"+newUser)
         added = {
             dateAdded : Date.now(),
-            
+
         }
         firebaseNewUser.update(added);
         firebaseValueCheck.once('value', function(snapshot) {
             console.log(snapshot.val().users.newUser)
-        }) 
+        })
 
-        currentUser = newUser       
-    }); 
+        currentUser = newUser
+    });
 });
 
-    
+
 
     window.fbAsyncInit = function() {
         FB.init({
             appId      : '1518819868427496',
             cookie     : true,  // enable cookies to allow the server to access the session
-                                                    
+
             xfbml      : true,  // parse social plugins on this page
             version    : 'v2.5' // use graph api version 2.5
         });
@@ -147,7 +147,7 @@ $('#modalNew').ready(function(){
 
 
         // The response object is returned with a status field that lets the app know the current login status of the person.
-         
+
         function checkLoginState() {
             FB.getLoginStatus(function(response) {
                 statusChangeCallback(response);
@@ -162,7 +162,7 @@ $('#modalNew').ready(function(){
                 testAPI();
 
             } else if (response.status === 'not_authorized') {
-                
+
                 document.getElementById('status').innerHTML = 'Please log ' +
                     'into this app.';
             } else {
@@ -175,17 +175,17 @@ $('#modalNew').ready(function(){
             if (response.status === 'connected') {
                 var accessToken = response.authResponse.accessToken;
                 console.log('you are logged in')
-                
-            } 
+
+            }
             statusChangeCallback(response);
         });
 
-    
+
     }; //close window.fbAsyncInit
 
-    
 
-    // Load the SDK asynchronously 
+
+    // Load the SDK asynchronously
     // (function(d, s, id) {
     //     var js, fjs = d.getElementsByTagName(s)[0];
     //     if (d.getElementById(id)) return;
@@ -195,7 +195,7 @@ $('#modalNew').ready(function(){
     // }(document, 'script', 'facebook-jssdk'));
 
     // FB Graph API
-    
+
 
 
     var userLikes = [];
@@ -203,25 +203,25 @@ $('#modalNew').ready(function(){
 
         FB.api('/me','GET', {"fields":"id,name,email,likes,friends,invitable_friends{id,picture,name},location"},function(response) {
                     console.log('This is FB Graph API response: ', response);
-                    
+
             fbPaging = response.invitable_friends.paging.next
                 // fbFriendImg = response.invitable_friends.data.picture.data.url
 
             fbAllFriendsList = fbPaging.replace('limit=25', 'limit=5000');
 
-                   
 
-            FB.api(fbAllFriendsList, function(response) {  
+
+            FB.api(fbAllFriendsList, function(response) {
                 console.log(response);
-                
+
                 for (x=0;x<response.data.length;x++) {
                     allFriends.push(response.data[x].name);
                     allFriendImg.push(response.data[x].picture.data.url)
 
 
                 }
-                
-                for(a=0;a<allFriends.length;a++){ 
+
+                for(a=0;a<allFriends.length;a++){
                     // debugger;
 
                     facebookUserProfile.userFriends[a] = [allFriends[a], allFriendImg[a]]
@@ -229,15 +229,15 @@ $('#modalNew').ready(function(){
 
                 var newFirebaseUser = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/"+facebookUserProfile.userName);
                 // debugger;
-                newFirebaseUser.set(facebookUserProfile);   
+                newFirebaseUser.set(facebookUserProfile);
 
             });
 
 
 
-                        
 
-            facebookUserProfile = { 
+
+            facebookUserProfile = {
                 userName: response.name,
                 userID: response.id,
                 userEmail: response.email,
@@ -250,38 +250,38 @@ $('#modalNew').ready(function(){
 
 
             var newFirebaseUser = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/"+response.name);
-            
-            newFirebaseUser.set(facebookUserProfile); 
-            
-            
 
-            if (response.likes.data.length != undefined) { 
-                
+            newFirebaseUser.set(facebookUserProfile);
+
+
+
+            if (response.likes.data.length != undefined) {
+
                 for (h=0;h<response.likes.data.length;h++) {
                 // var userLikes = []
                  userLikes.push(response.likes.data[h].name);
                 }
 
-                for(b=0;b<userLikes.length;b++){ 
+                for(b=0;b<userLikes.length;b++){
                     // debugger;
 
                     facebookUserProfile.userLikes[b] = userLikes[b]
                 }
 
                 var newFirebaseUser = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/"+facebookUserProfile.userName);
-            
-                newFirebaseUser.set(facebookUserProfile); 
+
+                newFirebaseUser.set(facebookUserProfile);
             }
 
             // where.push(response.location.name); //where your location is
 
-        }); 
+        });
 
     } //end Test api
-                
+
                 // FBwhere = where[0]; //this is a runYelpOnce() var
 
-                //  if (userLikes.length > 0) { 
+                //  if (userLikes.length > 0) {
                 //      for (k=0;k<what.length;k++) {
                 //          FBwhat = userLikes[k] //this is a runYelpOnce() var
                 //          runYelpOnce()
@@ -293,25 +293,25 @@ $('#modalNew').ready(function(){
 
 
 
-    // }    
-    
+    // }
 
 
-//****************************************** Yelp ******************************************************       
+
+//****************************************** Yelp ******************************************************
 
 function runYelp() {
 
     locations = [];
     activeMarkers = [];
     if (activeMarkers.length > 0){clearMarkers()};
-    
 
-    
+
+
     what = $('#what').val();
     where = $('#where').val();
-    
 
-        
+
+
 
     firebaseValueCheck.once('value', function(snapshot) {
             var auth = {
@@ -368,13 +368,13 @@ function runYelp() {
 
                 $('.panel-title').text('The best '+what+' spots in '+where+' are listed below').css('text-align', 'center');
                 $("#searches").addClass('table table-hover')
-                
+
                 var i;
 
                 for(i=0; i<=9; i++){
-                    
+
                     if (data.businesses[i].is_claimed) {
-                        var isClaimed = 'yes'       
+                        var isClaimed = 'yes'
                     }else{
                         isClaimed = 'no'
                     }
@@ -382,8 +382,8 @@ function runYelp() {
                     $("#searches").append("<tr class="+i+">"+'<td>'+'<a href='+data.businesses[i].url+">"+data.businesses[i].name +"</a>"+'</td>');
                     $("."+i).append('<td>'+'<img src='+ data.businesses[i].rating_img_url+'>'+'</td>');
                     $("."+i).append('<td>Phone: '+data.businesses[i].phone+'</td>');
-                    $("."+i).append('<td>Is this claimed by owner: '+isClaimed+'</td>'+'</tr>'); 
-                    $("."+i).append("<br />"); 
+                    $("."+i).append('<td>Is this claimed by owner: '+isClaimed+'</td>'+'</tr>');
+                    $("."+i).append("<br />");
                 };
 
                 var j;
@@ -397,12 +397,12 @@ function runYelp() {
 
 
             }
-        
+
         });
 
     });
 
- 
+
 } //end runYelp() *********************
 
 //************************ Google map api **************************************
@@ -417,8 +417,8 @@ var checkedPlaces = []
 
 
   //Get to users
-var currentUserFirebase = firebaseCountUp.child(currentUser);    //Get to current User 
-   
+var currentUserFirebase = firebaseCountUp.child(currentUser);    //Get to current User
+
 
 function updateCounter(){
 
@@ -429,16 +429,16 @@ function updateCounter(){
         }
         currentUserFirebase = firebaseCountUp.child(currentUser);
         currentUserFirebase.update(addCount)
-       
+
 
 };
 
 
 if (navigator.geolocation) {
-    
+
         navigator.geolocation.getCurrentPosition(getCoordinates);
         console.log("got it");
-    } else { 
+    } else {
         console.log("Geolocation is not supported by this browser.");
     };
 
@@ -468,12 +468,12 @@ function clearMarkers() {
     setMapOnAll(null)
 };
 
-function initMap() {    
+function initMap() {
       map = new google.maps.Map(
         document.getElementById('map'), {
           center: {lat: 40.728, lng: -74.078},
           zoom: 12
-        });     
+        });
 };
 
 function search(){
@@ -509,7 +509,7 @@ function geocodeAddress(locations, i) {
                 position: results[0].geometry.location,
                 title: title,
                 animation: google.maps.Animation.DROP,
-                address: address, 
+                address: address,
                 rating: rating,
                 phone: phone,
                 center: center
@@ -520,26 +520,30 @@ function geocodeAddress(locations, i) {
             bounds.extend(marker.getPosition());
             // map.setCenter(center);
             // map.panTo(center);
-            
+
         } else {
             alert ("geocode of" + address + "failed:" + status);
         }
       });
 };
 
-
+var thisPlace;
 function updatePlaced(){
-        
-        
+
+
         var addPlaces = {
-            
+
             places: {
-                0: checkedPlaces[0].title 
+                0: {
+                  0: checkedPlaces[0].title,
+                  1: checkedPlaces[0].address,
+                  2: checkedPlaces[0].phone
+                }
             }
-            
+
         }
         var firebasePlaceUp = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/"+currentUser);
-        
+
         firebasePlaceUp.once('value', function(snapshot){
             // debugger;
             if (snapshot.val().places == undefined){
@@ -549,21 +553,26 @@ function updatePlaced(){
                 for(i=0;i<snapshot.val().places.length;i++){
                     addPlaces = {
 
-                            [i+1]: checkedPlaces[0].title
-                        }                           
-                    } 
+                            [i+1]: {
+                              0: checkedPlaces[0].title,
+                              1: checkedPlaces[0].address,
+                              2: checkedPlaces[0].phone
+                        }
+                    }
 
                     var firebasePlaceDown = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/"+currentUser+"/places");
-                            
+
 
                     firebasePlaceDown.update(addPlaces)
                 }
 
 
-            
+
         });
-        
-        
+
+        $('#')
+
+
 
 };
 
@@ -571,7 +580,7 @@ function updatePlaced(){
 
 var infoBubble = null;
 
-var thisPlace;
+
 
 
 function infoWindow(marker, map, title, address, rating, phone){
@@ -592,7 +601,7 @@ function infoWindow(marker, map, title, address, rating, phone){
         infoBubble.open(map,marker);
 
         var checkBtn = htmls.find('button.checkIn')[0];
-        
+
 
         google.maps.event.addDomListener(checkBtn, "click", function(event) {
                 console.log("hi!");
@@ -606,15 +615,11 @@ function infoWindow(marker, map, title, address, rating, phone){
                   };
                   console.log(thisPlace);
                 checkedPlaces.unshift(thisPlace);
-                console.log(checkedPlaces); 
+                console.log(checkedPlaces);
                 updatePlaced();
         });
 
-        
+
 
     });
-} 
-
-
-
-
+}
