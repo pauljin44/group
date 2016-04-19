405b390cd084172ac872c86f72dcd26023199f39
kknoerz <kevink430@gmail.com>
2016-04-18 21:21:38 -0400

    changes

--- assets/javascript/ap.js
+++ assets/javascript/ap.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 //****************************************** Facebook SDK ******************************************************
 var firebaseValueCheck = new Firebase("https://sizzling-heat-1076.firebaseio.com/");
 var firebaseCountUp = new Firebase("https://sizzling-heat-1076.firebaseio.com/users/");
